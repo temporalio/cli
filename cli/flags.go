15ac237199252fb624acac8e7d5e812e868e61ed
Yichao Yang <yichao@temporal.io>
2022-03-25 17:07:33 -0700

    TerminateIfRunning workflowID reuse policy (#164)

--- cli/flags.go
+++ cli/flags.go
@@ -315,7 +315,7 @@ var flagsForRunWorkflow = []cli.Flag{
 	&cli.StringFlag{
 		Name: FlagWorkflowIDReusePolicy,
 		Usage: "Configure if the same workflow Id is allowed for use in new workflow execution. " +
-			"Options: AllowDuplicate, AllowDuplicateFailedOnly, RejectDuplicate",
+			"Options: AllowDuplicate, AllowDuplicateFailedOnly, RejectDuplicate, TerminateIfRunning",
 	},
 	&cli.StringSliceFlag{
 		Name:    FlagInput,
