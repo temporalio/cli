67ac851180dc6fdc37afe9c306062eff6c72d88a
feedmeapples <aksenov.ro@outlook.com>
2023-01-19 22:56:20 -0500

    .

--- install.sh
+++ install.sh
@@ -88,40 +88,6 @@ main() {
         fi
     fi
 
-    # check if we have to use /dev/tty to prompt the user
-    local need_tty=yes
-    for arg in "$@"; do
-        case "$arg" in
-        --help)
-            usage
-            exit 0
-            ;;
-        *)
-            OPTIND=1
-            if [ "${arg%%--*}" = "" ]; then
-                # Long option (other than --help);
-                # don't attempt to interpret it.
-                continue
-            fi
-            while getopts :hy sub_arg "$arg"; do
-                case "$sub_arg" in
-                h)
-                    usage
-                    exit 0
-                    ;;
-                y)
-                    # user wants to skip the prompt --
-                    # we don't need /dev/tty
-                    need_tty=no
-                    ;;
-                *) ;;
-
-                esac
-            done
-            ;;
-        esac
-    done
-
     if $_ansi_escapes_are_valid; then
         printf "\33[1minfo:\33[0m downloading Temporal CLI\n" 1>&2
     else
