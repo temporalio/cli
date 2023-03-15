c71c6f883f7596d32df944fdafa8eff65a94ad1b
Samantha Santiago <samantha@temporal.io>
2023-03-15 11:48:47 -0500

    passed custom template in BuildApp()

--- app/app.go
+++ app/app.go
@@ -41,6 +41,7 @@ func BuildApp() *cli.App {
 	app.Commands = commands(defaultCfg)
 	app.Before = configureCLI
 	app.ExitErrHandler = HandleError
+	app.CustomAppHelpTemplate = common.CustomTemplateHelpCLI
 
 	// set builder if not customized
 	if client.CFactory == nil {
