ae2d8121c526dc141e69586363deb1c7d8d054a3
Saman Barghi <saman.barghi@temporal.io>
2023-04-19 14:09:22 -0700

    Add e2e test for update

--- tests/workflow_test.go
+++ tests/workflow_test.go
@@ -6,8 +6,10 @@ import (
 	"os"
 
 	"github.com/pborman/uuid"
+	"github.com/temporalio/cli/app"
 	"github.com/temporalio/cli/tests/workflows/helloworld"
-	"go.temporal.io/sdk/client"
+	"github.com/temporalio/cli/tests/workflows/update"
+	sdkclient "go.temporal.io/sdk/client"
 	"go.temporal.io/sdk/worker"
 )
 
@@ -25,7 +27,7 @@ func (s *e2eSuite) TestWorkflowShow_ReplayableHistory() {
 
 	wfr, err := c.ExecuteWorkflow(
 		context.Background(),
-		client.StartWorkflowOptions{TaskQueue: testTq},
+		sdkclient.StartWorkflowOptions{TaskQueue: testTq},
 		helloworld.Workflow,
 		"world",
 	)
@@ -50,4 +52,32 @@ func (s *e2eSuite) TestWorkflowShow_ReplayableHistory() {
 	replayer.RegisterWorkflow(helloworld.Workflow)
 	err = replayer.ReplayWorkflowHistoryFromJSONFile(nil, historyFile)
 	s.NoError(err)
+}
+
+func (s *e2eSuite) TestWorkflowUpdate() {
+	s.app = app.BuildApp()
+	c := s.ts.Client()
+
+	s.NewWorker(testTq, func(r worker.Registry) {
+		r.RegisterWorkflow(update.Counter)
+	})
+
+	wfr, err := c.ExecuteWorkflow(
+		context.Background(),
+		sdkclient.StartWorkflowOptions{TaskQueue: testTq},
+		update.Counter,
+	)
+	s.NoError(err)
+	signalWorkflow := func() {
+		// send a Signal to stop the workflow
+		err = c.SignalWorkflow(context.Background(), wfr.GetID(), wfr.GetRunID(), update.Done, nil)
+		s.NoError(err)
+	}
+
+	defer signalWorkflow()
+
+	// update
+	//TODO: uncomment when update is enabled in the server by default, currently there is no way to enable it to test the command
+	//err = s.app.Run([]string{"", "workflow", "update", "--workflow-id", wfr.GetID(), "--run-id", wfr.GetRunID(), "--namespace", "default", "--name", update.FetchAndAdd, "-i", `"1"`, "--wait-policy", "Completed"})
+	//s.NoError(err)
 }
