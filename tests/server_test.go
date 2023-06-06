98f7dc84dbf488471370470d2a0813066691fbb2
feedmeapples <aksenov.ro@outlook.com>
2023-06-06 18:24:45 -0400

    fix server rc

--- tests/server_test.go
+++ tests/server_test.go
@@ -1,30 +1,9 @@
-// The MIT License
-//
-// Copyright (c) 2023 Temporal Technologies Inc.  All rights reserved.
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-
 package tests
 
 import (
 	"os"
 	"os/exec"
+	"runtime"
 	"syscall"
 	"testing"
 	"time"
@@ -33,16 +12,21 @@ import (
 )
 
 func TestServerInterruptRC(t *testing.T) {
+	exeName := "./test-temporal"
+	if runtime.GOOS == "windows" {
+		exeName += ".exe"
+	}
+
 	// build and run the binary, not "go run" as it modifies the exit code
-	build := exec.Command("go", "build", "-o", "./test-temporal", "../cmd/temporal")
+	build := exec.Command("go", "build", "-o", exeName, "../cmd/temporal")
 	build.Env = append(os.Environ(), "CGO_ENABLED=0")
 	err := build.Run()
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer os.Remove("./test-temporal")
+	defer os.Remove(exeName)
 
-	cmd := exec.Command("./test-temporal", "server", "start-dev", "--headless")
+	cmd := exec.Command(exeName, "server", "start-dev", "--headless")
 	err = cmd.Start()
 	if err != nil {
 		t.Fatal(err)
