484702f1cd671fceecab4e0c17ff2d0a3b635c3e
feedmeapples <aksenov.ro@outlook.com>
2023-06-15 13:23:55 -0400

    f2

--- tests/interrupt_windows.go
+++ tests/interrupt_windows.go
@@ -11,46 +11,17 @@ import (
 
 // sendInterrupt calls the break event on the given process for graceful shutdown.
 func sendInterrupt(process *os.Process) error {
-	dll, err := windows.LoadDLL("kernel32.dll")
+	dll, err := syscall.LoadDLL("kernel32.dll")
 	if err != nil {
 		return err
 	}
-	defer dll.Release()
-	f, err := dll.FindProc("AttachConsole")
+	f, err := d.FindProc("GenerateConsoleCtrlEvent")
 	if err != nil {
 		return err
 	}
-	r1, _, err := f.Call(uintptr(process.Pid))
-	if r1 == 0 && err != syscall.ERROR_ACCESS_DENIED {
+	r, _, err := p.Call(uintptr(syscall.CTRL_BREAK_EVENT), uintptr(process.Pid))
+	if r == 0 {
 		return err
 	}
-
-	f, err = dll.FindProc("SetConsoleCtrlHandler")
-	if err != nil {
-		return err
-	}
-	r1, _, err = f.Call(0, 1)
-	if r1 == 0 {
-		return err
-	}
-	f, err = dll.FindProc("GenerateConsoleCtrlEvent")
-	if err != nil {
-		return err
-	}
-	r1, _, err = f.Call(windows.CTRL_BREAK_EVENT, uintptr(process.Pid))
-	if r1 == 0 {
-		return err
-	}
-
-	// Free the console after sending the interrupt
-	f, err = dll.FindProc("FreeConsole")
-	if err != nil {
-		return err
-	}
-	r1, _, err = f.Call()
-	if r1 == 0 {
-		return err
-	}
-
 	return nil
 }
