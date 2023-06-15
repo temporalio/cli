cfdb622eefc896b51477bd976d1d3c6e66a0db9f
feedmeapples <aksenov.ro@outlook.com>
2023-06-15 18:14:34 -0400

    f3

--- tests/interrupt_windows.go
+++ tests/interrupt_windows.go
@@ -11,15 +11,15 @@ import (
 
 // sendInterrupt calls the break event on the given process for graceful shutdown.
 func sendInterrupt(process *os.Process) error {
-	dll, err := syscall.LoadDLL("kernel32.dll")
+	dll, err := windows.LoadDLL("kernel32.dll")
 	if err != nil {
 		return err
 	}
-	f, err := d.FindProc("GenerateConsoleCtrlEvent")
+	p, err := dll.FindProc("GenerateConsoleCtrlEvent")
 	if err != nil {
 		return err
 	}
-	r, _, err := p.Call(uintptr(syscall.CTRL_BREAK_EVENT), uintptr(process.Pid))
+	r, _, err := p.Call(uintptr(windows.CTRL_BREAK_EVENT), uintptr(process.Pid))
 	if r == 0 {
 		return err
 	}
