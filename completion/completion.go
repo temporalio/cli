e670b6f0e056ebc29884e8d8fd56ffc5facb5d48
David Reiss <david@temporal.io>
2023-07-07 09:43:57 -0700

    update completion scripts to the ones on the v2-maint branch of cli

--- completion/completion.go
+++ completion/completion.go
@@ -7,40 +7,52 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-// taken from https://github.com/urfave/cli/blob/master/autocomplete/zsh_autocomplete
+// taken from https://github.com/urfave/cli/blob/v2-maint/autocomplete/zsh_autocomplete
 var zsh_script = `#compdef temporal
 _cli_zsh_autocomplete() {
   local -a opts
   local cur
   cur=${words[-1]}
   if [[ "$cur" == "-"* ]]; then
-    opts=("${(@f)$(_CLI_ZSH_AUTOCOMPLETE_HACK=1 ${words[@]:0:#words[@]-1} ${cur} --generate-bash-completion)}")
+    opts=("${(@f)$(${words[@]:0:#words[@]-1} ${cur} --generate-bash-completion)}")
   else
-    opts=("${(@f)$(_CLI_ZSH_AUTOCOMPLETE_HACK=1 ${words[@]:0:#words[@]-1} --generate-bash-completion)}")
+    opts=("${(@f)$(${words[@]:0:#words[@]-1} --generate-bash-completion)}")
   fi
   if [[ "${opts[1]}" != "" ]]; then
     _describe 'values' opts
   else
     _files
   fi
-  return
 }
 compdef _cli_zsh_autocomplete temporal
 `
 
-// taken from https://github.com/urfave/cli/blob/master/autocomplete/bash_autocomplete
+// taken from https://github.com/urfave/cli/blob/v2-maint/autocomplete/bash_autocomplete
 var bash_script = `#! /bin/bash
+# Macs have bash3 for which the bash-completion package doesn't include
+# _init_completion. This is a minimal version of that function.
+_cli_init_completion() {
+  COMPREPLY=()
+  _get_comp_words_by_ref "$@" cur prev words cword
+}
 _cli_bash_autocomplete() {
   if [[ "${COMP_WORDS[0]}" != "source" ]]; then
-    local cur opts base
+    local cur opts base words
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
-    if [[ "$cur" == "-"* ]]; then
-      opts=$( ${COMP_WORDS[@]:0:$COMP_CWORD} ${cur} --generate-bash-completion )
+    if declare -F _init_completion >/dev/null 2>&1; then
+      _init_completion -n "=:" || return
     else
-      opts=$( ${COMP_WORDS[@]:0:$COMP_CWORD} --generate-bash-completion )
+      _cli_init_completion -n "=:" || return
     fi
-    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    words=("${words[@]:0:$cword}")
+    if [[ "$cur" == "-"* ]]; then
+      requestComp="${words[*]} ${cur} --generate-bash-completion"
+    else
+      requestComp="${words[*]} --generate-bash-completion"
+    fi
+    opts=$(eval "${requestComp}" 2>/dev/null)
+    COMPREPLY=($(compgen -W "${opts}" -- ${cur}))
     return 0
   fi
 }
