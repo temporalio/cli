6694082ab00b96bcabc13728ddfb71c4a79e3750
Ruslan <11838981+feedmeapples@users.noreply.github.com>
2023-07-18 19:58:43 -0700

    Fix batch signal's processing of input values (#287)
    * Fix batch signal's processing of input values
    
    * .
    
    * .

--- tests/workflow_test.go
+++ tests/workflow_test.go
@@ -181,10 +181,9 @@ func (s *e2eSuite) TestWorkflowSignal_Batch() {
 		_ = testserver.Stop()
 	}()
 
-	w := s.newWorker(testserver, testTq, func(r worker.Registry) {
+	defer s.newWorker(testserver, testTq, func(r worker.Registry) {
 		r.RegisterWorkflow(awaitsignal.Workflow)
-	})
-	defer w.Stop()
+	}).Stop()
 
 	c := testserver.Client()
 
@@ -208,7 +207,9 @@ func (s *e2eSuite) TestWorkflowSignal_Batch() {
 		return len(wfs.GetExecutions()) == 3
 	}, 10*time.Second, time.Second)
 
-	err := app.Run([]string{"", "workflow", "signal", "--name", awaitsignal.Done, "--query", "WorkflowId = '1' OR WorkflowId = '2'", "--reason", "test", "--yes", "--namespace", testNamespace})
+	i1 := "\"" + awaitsignal.Input1 + "\""
+	q := "WorkflowId = '1' OR WorkflowId = '2'"
+	err := app.Run([]string{"", "workflow", "signal", "--name", awaitsignal.Done, "--input", i1, "--query", q, "--reason", "test", "--yes", "--namespace", testNamespace})
 	s.NoError(err)
 
 	awaitTaskQueuePoller(s, c, testTq)
