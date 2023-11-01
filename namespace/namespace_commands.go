b96845c64e6dd0f4c2ad498d2ac51372d34d9f44
Josh Berry <josh.berry@temporal.io>
2023-11-01 15:37:37 -0700

    Print `NamespaceInfo.Data` when describing a namespace (#372)
    Closes #363.

--- namespace/namespace_commands.go
+++ namespace/namespace_commands.go
@@ -328,7 +328,7 @@ func DeleteNamespace(c *cli.Context) error {
 
 func printNamespace(c *cli.Context, resp *workflowservice.DescribeNamespaceResponse) error {
 	po := &output.PrintOptions{
-		Fields:       []string{"NamespaceInfo.Name", "NamespaceInfo.Id", "NamespaceInfo.Description", "NamespaceInfo.OwnerEmail", "NamespaceInfo.State", "Config.WorkflowExecutionRetentionTtl", "ReplicationConfig.ActiveClusterName", "ReplicationConfig.Clusters", "Config.HistoryArchivalState", "Config.VisibilityArchivalState", "IsGlobalNamespace", "FailoverVersion", "FailoverHistory"},
+		Fields:       []string{"NamespaceInfo.Name", "NamespaceInfo.Id", "NamespaceInfo.Description", "NamespaceInfo.OwnerEmail", "NamespaceInfo.State", "NamespaceInfo.Data", "Config.WorkflowExecutionRetentionTtl", "ReplicationConfig.ActiveClusterName", "ReplicationConfig.Clusters", "Config.HistoryArchivalState", "Config.VisibilityArchivalState", "IsGlobalNamespace", "FailoverVersion", "FailoverHistory"},
 		FieldsLong:   []string{"Config.HistoryArchivalUri", "Config.VisibilityArchivalUri"},
 		OutputFormat: output.Card,
 	}
