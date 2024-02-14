baa9e303a522229b8f77445e5c88311e47f355be
Chad Retz <chad@temporal.io>
2024-02-14 07:59:48 -0600

    Embed fallback timezone DB

--- cmd/temporal/main.go
+++ cmd/temporal/main.go
@@ -4,6 +4,9 @@ import (
 	"context"
 
 	"github.com/temporalio/cli/temporalcli"
+
+	// Embed time zone database as a fallback if platform database can't be found
+	_ "time/tzdata"
 )
 
 func main() {
