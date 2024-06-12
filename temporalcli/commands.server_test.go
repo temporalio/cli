f98c0aad08f7bea25ae809b79385c5adbd34e012
Chad Retz <chad@temporal.io>
2024-06-12 09:30:57 -0500

    Accept search attributes for dev server and disable cache by default
    Fixes #21

--- temporalcli/commands.server_test.go
+++ temporalcli/commands.server_test.go
@@ -11,7 +11,9 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/temporalio/cli/temporalcli/devserver"
+	"go.temporal.io/api/operatorservice/v1"
 	"go.temporal.io/sdk/client"
+	"go.temporal.io/sdk/temporal"
 )
 
 // TODO(cretz): To test:
@@ -120,6 +122,82 @@ func TestServer_StartDev_ConcurrentStarts(t *testing.T) {
 		}()
 	}
 	wg.Wait()
+}
+
+func TestServer_StartDev_WithSearchAttributes(t *testing.T) {
+	h := NewCommandHarness(t)
+	defer h.Close()
+
+	// Start in background, then wait for client to be able to connect
+	port := strconv.Itoa(devserver.MustGetFreePort("127.0.0.1"))
+	resCh := make(chan *CommandResult, 1)
+	go func() {
+		resCh <- h.Execute(
+			"server", "start-dev",
+			"-p", port,
+			"--headless",
+			"--search-attribute", "search-attr-1=Int",
+			"--search-attribute", "search-attr-2=kEyWoRdLiSt",
+		)
+	}()
+
+	// Try to connect for a bit while checking for error
+	var cl client.Client
+	h.EventuallyWithT(func(t *assert.CollectT) {
+		select {
+		case res := <-resCh:
+			if res.Err != nil {
+				panic(res.Err)
+			}
+		default:
+		}
+		var err error
+		cl, err = client.Dial(client.Options{HostPort: "127.0.0.1:" + port})
+		if !assert.NoError(t, err) {
+			return
+		}
+		// Confirm search attributes are present
+		resp, err := cl.OperatorService().ListSearchAttributes(
+			context.Background(), &operatorservice.ListSearchAttributesRequest{Namespace: "default"})
+		if !assert.NoError(t, err) {
+			return
+		}
+		assert.Contains(t, resp.CustomAttributes, "search-attr-1")
+		assert.Contains(t, resp.CustomAttributes, "search-attr-2")
+
+	}, 3*time.Second, 200*time.Millisecond)
+	defer cl.Close()
+
+	// Do a workflow start with the search attributes
+	run, err := cl.ExecuteWorkflow(
+		context.Background(),
+		client.StartWorkflowOptions{
+			TaskQueue: "my-task-queue",
+			TypedSearchAttributes: temporal.NewSearchAttributes(
+				temporal.NewSearchAttributeKeyInt64("search-attr-1").ValueSet(123),
+				temporal.NewSearchAttributeKeyKeywordList("search-attr-2").ValueSet([]string{"foo", "bar"}),
+			),
+		},
+		"MyWorkflow",
+	)
+	h.NoError(err)
+	h.NotEmpty(run.GetRunID())
+
+	// Check that they are there
+	desc, err := cl.DescribeWorkflowExecution(context.Background(), run.GetID(), "")
+	h.NoError(err)
+	sa := desc.WorkflowExecutionInfo.SearchAttributes.IndexedFields
+	h.JSONEq("123", string(sa["search-attr-1"].Data))
+	h.JSONEq(`["foo","bar"]`, string(sa["search-attr-2"].Data))
+
+	// Send an interrupt by cancelling context
+	h.CancelContext()
+	select {
+	case <-time.After(20 * time.Second):
+		h.Fail("didn't cleanup after 20 seconds")
+	case res := <-resCh:
+		h.NoError(res.Err)
+	}
 }
 
 type testLogger struct {
