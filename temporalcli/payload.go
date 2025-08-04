daf3c284d11367ee76deb48126691fa33e32728d
Roey Berman <roey@temporal.io>
2025-08-04 15:40:50 -0600

    Support multiple --input-meta flags for the same key (#836)
    Closes #833
    
    ---------
    
    Co-authored-by: Spencer Judge <sjudge@hey.com>

--- temporalcli/payload.go
+++ temporalcli/payload.go
@@ -9,12 +9,25 @@ import (
 	"go.temporal.io/api/common/v1"
 )
 
-func CreatePayloads(data [][]byte, metadata map[string][]byte, isBase64 bool) (*common.Payloads, error) {
+// CreatePayloads creates API Payload objects from given data and metadata slices.
+// If metadata has an entry at a data index, it is used, otherwise it uses the metadata entry at index 0.
+func CreatePayloads(data [][]byte, metadata map[string][][]byte, isBase64 bool) (*common.Payloads, error) {
 	ret := &common.Payloads{Payloads: make([]*common.Payload, len(data))}
 	for i, in := range data {
-		// If it's JSON, validate it
-		if strings.HasPrefix(string(metadata["encoding"]), "json/") && !json.Valid(in) {
-			return nil, fmt.Errorf("input #%v is not valid JSON", i+1)
+		var metadataForIndex = make(map[string][]byte, len(metadata))
+		for k, vals := range metadata {
+			if len(vals) == 0 {
+				continue
+			}
+			v := vals[0]
+			if len(vals) > i {
+				v = vals[i]
+			}
+			// If it's JSON, validate it
+			if k == "encoding" && strings.HasPrefix(string(v), "json/") && !json.Valid(in) {
+				return nil, fmt.Errorf("input #%v is not valid JSON", i+1)
+			}
+			metadataForIndex[k] = v
 		}
 		// Decode base64 if base64'd (std encoding only for now)
 		if isBase64 {
@@ -23,7 +36,7 @@ func CreatePayloads(data [][]byte, metadata map[string][]byte, isBase64 bool) (*
 				return nil, fmt.Errorf("input #%v is not valid base64", i+1)
 			}
 		}
-		ret.Payloads[i] = &common.Payload{Data: in, Metadata: metadata}
+		ret.Payloads[i] = &common.Payload{Data: in, Metadata: metadataForIndex}
 	}
 	return ret, nil
 }
