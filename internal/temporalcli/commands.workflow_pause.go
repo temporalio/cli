4b2c8757cc436ea55fd910a40619e43a101889f9
Chetan Gowda <gow@users.noreply.github.com>
2025-12-15 13:30:04 -0800

    Add pause/unpause commands (#885)
    Redoing https://github.com/temporalio/cli/pull/885/
    <!--- Note to EXTERNAL Contributors -->
    <!-- Thanks for opening a PR!
    If it is a significant code change, please **make sure there is an open
    issue** for this.
    We work best with you when we have accepted the idea first before you
    code. -->
    
    <!--- For ALL Contributors 👇 -->
    
    Added 2 new commands (experimental)
    - temporal workflow pause
    - temporal workflow unpause
    
    Did a bunch of unrelated cleanup as well.
    
    Need these commands to allow pausing and unpausing of workflows via cli
    
    <!--- add/delete as needed --->
    
    1. Closes <!-- add issue number here -->
    
    2. How was this tested:
    <!--- Please describe how you tested your changes/how we can test them
    -->
    
    3. Any docs updates needed?
    <!--- update README if applicable
          or point out where to update docs.temporal.io -->

---
+++ internal/temporalcli/commands.workflow_pause.go
@@ -0,0 +1,48 @@
+package temporalcli
+
+import (
+	"go.temporal.io/api/workflowservice/v1"
+)
+
+func (c *TemporalWorkflowPauseCommand) run(cctx *CommandContext, args []string) error {
+	cl, err := dialClient(cctx, &c.Parent.ClientOptions)
+	if err != nil {
+		return err
+	}
+	defer cl.Close()
+
+	_, err = cl.WorkflowService().PauseWorkflowExecution(cctx, &workflowservice.PauseWorkflowExecutionRequest{
+		Namespace:  c.Parent.Namespace,
+		Reason:     c.Reason,
+		WorkflowId: c.WorkflowId,
+		RunId:      c.RunId,
+		Identity:   c.Parent.Identity,
+	})
+	if err != nil {
+		return err
+	}
+
+	cctx.Printer.Println("Workflow Execution paused")
+	return nil
+}
+
+func (c *TemporalWorkflowUnpauseCommand) run(cctx *CommandContext, args []string) error {
+	cl, err := dialClient(cctx, &c.Parent.ClientOptions)
+	if err != nil {
+		return err
+	}
+	defer cl.Close()
+
+	_, err = cl.WorkflowService().UnpauseWorkflowExecution(cctx, &workflowservice.UnpauseWorkflowExecutionRequest{
+		Namespace:  c.Parent.Namespace,
+		Reason:     c.Reason,
+		WorkflowId: c.WorkflowId,
+		RunId:      c.RunId,
+		Identity:   c.Parent.Identity,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
