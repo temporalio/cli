054f836265904ac8e4a2aba725620c9716355124
Carly de Frondeville <cdefrondeville@berkeley.edu>
2026-02-02 21:34:22 -0800

    switch to grpc instead of sdk

--- internal/temporalcli/commands.worker.deployment.go
+++ internal/temporalcli/commands.worker.deployment.go
@@ -8,7 +8,11 @@ import (
 	"github.com/fatih/color"
 	"github.com/temporalio/cli/internal/printer"
 	"go.temporal.io/api/common/v1"
+	deploymentpb "go.temporal.io/api/deployment/v1"
+	enumspb "go.temporal.io/api/enums/v1"
 	"go.temporal.io/api/serviceerror"
+	taskqueuepb "go.temporal.io/api/taskqueue/v1"
+	"go.temporal.io/api/workflowservice/v1"
 	"go.temporal.io/sdk/client"
 	"go.temporal.io/sdk/worker"
 )
@@ -257,51 +261,72 @@ func printWorkerDeploymentInfo(cctx *CommandContext, deploymentInfo client.Worke
 	return cctx.Printer.PrintStructured(fDeploymentInfo, printer.StructuredOptions{})
 }
 
-func formatDrainageInfo(drainageInfo *client.WorkerDeploymentVersionDrainageInfo) (formattedDrainageInfo, error) {
-	if drainageInfo == nil {
-		return formattedDrainageInfo{}, nil
-	}
+// Proto-based conversion functions for describe-version command
+// These functions convert gRPC proto types directly, avoiding SDK type dependencies.
 
-	drainageStr, err := drainageStatusToStr(drainageInfo.DrainageStatus)
-	if err != nil {
-		return formattedDrainageInfo{}, err
+func drainageStatusProtoToStr(status enumspb.VersionDrainageStatus) (string, error) {
+	switch status {
+	case enumspb.VERSION_DRAINAGE_STATUS_UNSPECIFIED:
+		return "unspecified", nil
+	case enumspb.VERSION_DRAINAGE_STATUS_DRAINING:
+		return "draining", nil
+	case enumspb.VERSION_DRAINAGE_STATUS_DRAINED:
+		return "drained", nil
+	default:
+		return "", fmt.Errorf("unrecognized drainage status: %d", status)
 	}
-
-	return formattedDrainageInfo{
-		DrainageStatus:  drainageStr,
-		LastChangedTime: drainageInfo.LastChangedTime,
-		LastCheckedTime: drainageInfo.LastCheckedTime,
-	}, nil
 }
 
-func formatTaskQueuesInfos(tqis []client.WorkerDeploymentTaskQueueInfo, includeStats bool) ([]formattedTaskQueueInfoRowType, error) {
+func taskQueueTypeProtoToStr(taskQueueType enumspb.TaskQueueType) (string, error) {
+	switch taskQueueType {
+	case enumspb.TASK_QUEUE_TYPE_UNSPECIFIED:
+		return "unspecified", nil
+	case enumspb.TASK_QUEUE_TYPE_WORKFLOW:
+		return "workflow", nil
+	case enumspb.TASK_QUEUE_TYPE_ACTIVITY:
+		return "activity", nil
+	case enumspb.TASK_QUEUE_TYPE_NEXUS:
+		return "nexus", nil
+	default:
+		return "", fmt.Errorf("unrecognized task queue type: %d", taskQueueType)
+	}
+}
+
+func formatVersionStatsProto(tqStats *taskqueuepb.TaskQueueStats) formattedVersionStatsRowType {
+	if tqStats == nil {
+		return formattedVersionStatsRowType{}
+	}
+	return formattedVersionStatsRowType{
+		ApproximateBacklogCount: tqStats.ApproximateBacklogCount,
+		ApproximateBacklogAge:   tqStats.ApproximateBacklogAge.AsDuration(),
+		// BacklogIncreaseRate is computed as TasksAddRate - TasksDispatchRate (same as SDK)
+		BacklogIncreaseRate: tqStats.TasksAddRate - tqStats.TasksDispatchRate,
+		TasksAddRate:        tqStats.TasksAddRate,
+		TasksDispatchRate:   tqStats.TasksDispatchRate,
+	}
+}
+
+func formatTaskQueuesInfosProto(tqis []*workflowservice.DescribeWorkerDeploymentVersionResponse_VersionTaskQueue, includeStats bool) ([]formattedTaskQueueInfoRowType, error) {
 	var tqiRows []formattedTaskQueueInfoRowType
 	for _, tqi := range tqis {
-		tqTypeStr, err := taskQueueTypeToStr(tqi.Type)
+		tqTypeStr, err := taskQueueTypeProtoToStr(tqi.GetType())
 		if err != nil {
 			return tqiRows, err
 		}
 
 		row := formattedTaskQueueInfoRowType{
-			Name: tqi.Name,
+			Name: tqi.GetName(),
 			Type: tqTypeStr,
 		}
 
 		if includeStats {
-			fVersionStats, err := formatVersionStatsRowType(tqi.Stats)
-			if err != nil {
-				return tqiRows, err
-			}
+			fVersionStats := formatVersionStatsProto(tqi.GetStats())
 			row.Stats = &fVersionStats
 
-			if len(tqi.StatsByPriorityKey) > 0 {
+			if len(tqi.GetStatsByPriorityKey()) > 0 {
 				fVersionStatsByPriorityKey := map[int32]formattedVersionStatsRowType{}
-				for k, v := range tqi.StatsByPriorityKey {
-					formatted, err := formatVersionStatsRowType(&v)
-					if err != nil {
-						return tqiRows, err
-					}
-					fVersionStatsByPriorityKey[k] = formatted
+				for k, v := range tqi.GetStatsByPriorityKey() {
+					fVersionStatsByPriorityKey[k] = formatVersionStatsProto(v)
 				}
 				row.StatsByPriorityKey = fVersionStatsByPriorityKey
 			}
@@ -312,56 +337,52 @@ func formatTaskQueuesInfos(tqis []client.WorkerDeploymentTaskQueueInfo, includeS
 	return tqiRows, nil
 }
 
-func formatVersionStatsRowType(tqStats *client.TaskQueueStats) (formattedVersionStatsRowType, error) {
-	if tqStats == nil {
-		return formattedVersionStatsRowType{}, nil
+func formatDrainageInfoProto(drainageInfo *deploymentpb.VersionDrainageInfo) (formattedDrainageInfo, error) {
+	if drainageInfo == nil {
+		return formattedDrainageInfo{}, nil
 	}
-	return formattedVersionStatsRowType{
-		ApproximateBacklogCount: tqStats.ApproximateBacklogCount,
-		ApproximateBacklogAge:   tqStats.ApproximateBacklogAge,
-		BacklogIncreaseRate:     tqStats.BacklogIncreaseRate,
-		TasksAddRate:            tqStats.TasksAddRate,
-		TasksDispatchRate:       tqStats.TasksDispatchRate,
+
+	drainageStr, err := drainageStatusProtoToStr(drainageInfo.GetStatus())
+	if err != nil {
+		return formattedDrainageInfo{}, err
+	}
+
+	return formattedDrainageInfo{
+		DrainageStatus:  drainageStr,
+		LastChangedTime: drainageInfo.GetLastChangedTime().AsTime(),
+		LastCheckedTime: drainageInfo.GetLastCheckedTime().AsTime(),
 	}, nil
 }
 
-// workerDeploymentVersionInfoToRows converts SDK types to formatted types for display.
-// The taskQueueInfos parameter should be from resp.TaskQueueInfos (not resp.Info.TaskQueuesInfos)
-// because the stats are only populated in the top-level TaskQueueInfos field.
-func workerDeploymentVersionInfoToRows(deploymentInfo client.WorkerDeploymentVersionInfo, taskQueueInfos []client.WorkerDeploymentTaskQueueInfo, includeStats bool) (formattedWorkerDeploymentVersionInfoType, error) {
-	tqi, err := formatTaskQueuesInfos(taskQueueInfos, includeStats)
+// workerDeploymentVersionInfoProtoToRows converts gRPC proto types to formatted types for display.
+func workerDeploymentVersionInfoProtoToRows(deploymentInfo *deploymentpb.WorkerDeploymentVersionInfo, taskQueueInfos []*workflowservice.DescribeWorkerDeploymentVersionResponse_VersionTaskQueue, includeStats bool) (formattedWorkerDeploymentVersionInfoType, error) {
+	tqi, err := formatTaskQueuesInfosProto(taskQueueInfos, includeStats)
 	if err != nil {
 		return formattedWorkerDeploymentVersionInfoType{}, err
 	}
 
-	drainage, err := formatDrainageInfo(deploymentInfo.DrainageInfo)
+	drainage, err := formatDrainageInfoProto(deploymentInfo.GetDrainageInfo())
 	if err != nil {
 		return formattedWorkerDeploymentVersionInfoType{}, err
 	}
 
 	return formattedWorkerDeploymentVersionInfoType{
-		DeploymentName:     deploymentInfo.Version.DeploymentName,
-		BuildID:            deploymentInfo.Version.BuildID,
-		CreateTime:         deploymentInfo.CreateTime,
-		RoutingChangedTime: deploymentInfo.RoutingChangedTime,
-		CurrentSinceTime:   deploymentInfo.CurrentSinceTime,
-		RampingSinceTime:   deploymentInfo.RampingSinceTime,
-		RampPercentage:     deploymentInfo.RampPercentage,
+		DeploymentName:     deploymentInfo.GetDeploymentVersion().GetDeploymentName(),
+		BuildID:            deploymentInfo.GetDeploymentVersion().GetBuildId(),
+		CreateTime:         deploymentInfo.GetCreateTime().AsTime(),
+		RoutingChangedTime: deploymentInfo.GetRoutingChangedTime().AsTime(),
+		CurrentSinceTime:   deploymentInfo.GetCurrentSinceTime().AsTime(),
+		RampingSinceTime:   deploymentInfo.GetRampingSinceTime().AsTime(),
+		RampPercentage:     deploymentInfo.GetRampPercentage(),
 		DrainageInfo:       drainage,
 		TaskQueuesInfos:    tqi,
-		Metadata:           deploymentInfo.Metadata,
+		Metadata:           deploymentInfo.GetMetadata().GetEntries(),
 	}, nil
 }
 
-type printVersionInfoOptions struct {
-	showStats bool
-}
-
-// printWorkerDeploymentVersionInfo prints worker deployment version info.
-// The taskQueueInfos parameter should be from resp.TaskQueueInfos (not resp.Info.TaskQueuesInfos)
-// because the stats are only populated in the top-level TaskQueueInfos field.
-func printWorkerDeploymentVersionInfo(cctx *CommandContext, deploymentInfo client.WorkerDeploymentVersionInfo, taskQueueInfos []client.WorkerDeploymentTaskQueueInfo, msg string, opts printVersionInfoOptions) error {
-	fDeploymentInfo, err := workerDeploymentVersionInfoToRows(deploymentInfo, taskQueueInfos, opts.showStats)
+// printWorkerDeploymentVersionInfoProto prints worker deployment version info from proto types.
+func printWorkerDeploymentVersionInfoProto(cctx *CommandContext, deploymentInfo *deploymentpb.WorkerDeploymentVersionInfo, taskQueueInfos []*workflowservice.DescribeWorkerDeploymentVersionResponse_VersionTaskQueue, msg string, opts printVersionInfoOptions) error {
+	fDeploymentInfo, err := workerDeploymentVersionInfoProtoToRows(deploymentInfo, taskQueueInfos, opts.showStats)
 	if err != nil {
 		return err
 	}
@@ -371,13 +392,13 @@ func printWorkerDeploymentVersionInfo(cctx *CommandContext, deploymentInfo clien
 		var drainageStr string
 		var drainageLastChangedTime time.Time
 		var drainageLastCheckedTime time.Time
-		if deploymentInfo.DrainageInfo != nil {
-			drainageStr, err = drainageStatusToStr(deploymentInfo.DrainageInfo.DrainageStatus)
+		if deploymentInfo.GetDrainageInfo() != nil {
+			drainageStr, err = drainageStatusProtoToStr(deploymentInfo.GetDrainageInfo().GetStatus())
 			if err != nil {
 				return err
 			}
-			drainageLastChangedTime = deploymentInfo.DrainageInfo.LastChangedTime
-			drainageLastCheckedTime = deploymentInfo.DrainageInfo.LastCheckedTime
+			drainageLastChangedTime = deploymentInfo.GetDrainageInfo().GetLastChangedTime().AsTime()
+			drainageLastCheckedTime = deploymentInfo.GetDrainageInfo().GetLastCheckedTime().AsTime()
 		}
 
 		printMe := struct {
@@ -393,17 +414,17 @@ func printWorkerDeploymentVersionInfo(cctx *CommandContext, deploymentInfo clien
 			DrainageLastCheckedTime time.Time                  `cli:",cardOmitEmpty"`
 			Metadata                map[string]*common.Payload `cli:",cardOmitEmpty"`
 		}{
-			DeploymentName:          deploymentInfo.Version.DeploymentName,
-			BuildID:                 deploymentInfo.Version.BuildID,
-			CreateTime:              deploymentInfo.CreateTime,
-			RoutingChangedTime:      deploymentInfo.RoutingChangedTime,
-			CurrentSinceTime:        deploymentInfo.CurrentSinceTime,
-			RampingSinceTime:        deploymentInfo.RampingSinceTime,
-			RampPercentage:          deploymentInfo.RampPercentage,
+			DeploymentName:          deploymentInfo.GetDeploymentVersion().GetDeploymentName(),
+			BuildID:                 deploymentInfo.GetDeploymentVersion().GetBuildId(),
+			CreateTime:              deploymentInfo.GetCreateTime().AsTime(),
+			RoutingChangedTime:      deploymentInfo.GetRoutingChangedTime().AsTime(),
+			CurrentSinceTime:        deploymentInfo.GetCurrentSinceTime().AsTime(),
+			RampingSinceTime:        deploymentInfo.GetRampingSinceTime().AsTime(),
+			RampPercentage:          deploymentInfo.GetRampPercentage(),
 			DrainageStatus:          drainageStr,
 			DrainageLastChangedTime: drainageLastChangedTime,
 			DrainageLastCheckedTime: drainageLastCheckedTime,
-			Metadata:                deploymentInfo.Metadata,
+			Metadata:                deploymentInfo.GetMetadata().GetEntries(),
 		}
 		err := cctx.Printer.PrintStructured(printMe, printer.StructuredOptions{})
 		if err != nil {
@@ -421,6 +442,10 @@ func printWorkerDeploymentVersionInfo(cctx *CommandContext, deploymentInfo clien
 
 	// json output
 	return cctx.Printer.PrintStructured(fDeploymentInfo, printer.StructuredOptions{})
+}
+
+type printVersionInfoOptions struct {
+	showStats bool
 }
 
 func formatDurationShort(d time.Duration) string {
@@ -460,28 +485,28 @@ func printTaskQueuesInfo(cctx *CommandContext, taskQueues []formattedTaskQueueIn
 			if len(tq.StatsByPriorityKey) == 0 {
 				continue
 			}
-				cctx.Printer.Println()
-				cctx.Printer.Println(color.MagentaString(fmt.Sprintf("Stats by Priority (%s / %s):", tq.Name, tq.Type)))
+			cctx.Printer.Println()
+			cctx.Printer.Println(color.MagentaString(fmt.Sprintf("Stats by Priority (%s / %s):", tq.Name, tq.Type)))
 
-				// Sort priority keys for consistent output
-				priorities := make([]int32, 0, len(tq.StatsByPriorityKey))
-				for p := range tq.StatsByPriorityKey {
-					priorities = append(priorities, p)
-				}
-				sortInt32s(priorities)
+			// Sort priority keys for consistent output
+			priorities := make([]int32, 0, len(tq.StatsByPriorityKey))
+			for p := range tq.StatsByPriorityKey {
+				priorities = append(priorities, p)
+			}
+			sortInt32s(priorities)
 
-				priorityRows := make([]priorityStatsDisplayRow, 0, len(priorities))
-				for _, p := range priorities {
-					stats := tq.StatsByPriorityKey[p]
-					priorityRows = append(priorityRows, priorityStatsDisplayRow{
-						Priority:                p,
-						ApproximateBacklogCount: stats.ApproximateBacklogCount,
-						ApproximateBacklogAge:   formatDurationShort(stats.ApproximateBacklogAge),
-						BacklogIncreaseRate:     stats.BacklogIncreaseRate,
-						TasksAddRate:            stats.TasksAddRate,
-						TasksDispatchRate:       stats.TasksDispatchRate,
-					})
-				}
+			priorityRows := make([]priorityStatsDisplayRow, 0, len(priorities))
+			for _, p := range priorities {
+				stats := tq.StatsByPriorityKey[p]
+				priorityRows = append(priorityRows, priorityStatsDisplayRow{
+					Priority:                p,
+					ApproximateBacklogCount: stats.ApproximateBacklogCount,
+					ApproximateBacklogAge:   formatDurationShort(stats.ApproximateBacklogAge),
+					BacklogIncreaseRate:     stats.BacklogIncreaseRate,
+					TasksAddRate:            stats.TasksAddRate,
+					TasksDispatchRate:       stats.TasksDispatchRate,
+				})
+			}
 			if err := cctx.Printer.PrintStructured(priorityRows, printer.StructuredOptions{Table: &printer.TableOptions{}}); err != nil {
 				return fmt.Errorf("displaying priority stats failed: %w", err)
 			}
@@ -757,18 +782,21 @@ func (c *TemporalWorkerDeploymentDescribeVersionCommand) run(cctx *CommandContex
 	}
 	defer cl.Close()
 
-	dHandle := cl.WorkerDeploymentClient().GetHandle(c.DeploymentName)
-
-	resp, err := dHandle.DescribeVersion(cctx, client.WorkerDeploymentDescribeVersionOptions{
-		BuildID:              c.BuildId,
+	// Use raw gRPC instead of SDK's DeploymentClient to avoid circular dependency
+	// with SDK release that exposes TaskQueuesInfos in DescribeVersionResponse
+	resp, err := cl.WorkflowService().DescribeWorkerDeploymentVersion(cctx, &workflowservice.DescribeWorkerDeploymentVersionRequest{
+		Namespace: c.Parent.Parent.Namespace,
+		DeploymentVersion: &deploymentpb.WorkerDeploymentVersion{
+			DeploymentName: c.DeploymentName,
+			BuildId:        c.BuildId,
+		},
 		ReportTaskQueueStats: c.ReportTaskQueueStats,
 	})
 	if err != nil {
 		return fmt.Errorf("error describing worker deployment version: %w", err)
 	}
 
-	// Use resp.TaskQueueInfos (not resp.Info.TaskQueuesInfos) because stats are only in the top-level field
-	err = printWorkerDeploymentVersionInfo(cctx, resp.Info, resp.TaskQueueInfos, "Worker Deployment Version:", printVersionInfoOptions{
+	err = printWorkerDeploymentVersionInfoProto(cctx, resp.GetWorkerDeploymentVersionInfo(), resp.GetVersionTaskQueues(), "Worker Deployment Version:", printVersionInfoOptions{
 		showStats: c.ReportTaskQueueStats,
 	})
 	if err != nil {
