fb9eb91c2518227ead3dad1a32493ea7cc2d04c1
Nasit Sarwar Sony <nasitsony96@gmail.com>
2026-05-23 11:02:22 -0700

    Prefix dev server cluster ID with 'dev-server-' and add env var override
    - Prefix generated cluster IDs with 'dev-server-' for better identification
    - Add TEMPORAL_DEV_SERVER_CLUSTER_ID env var to allow override
    
    Fixes #609

--- internal/temporalcli/commands.server.go
+++ internal/temporalcli/commands.server.go
@@ -3,6 +3,7 @@ package temporalcli
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/google/uuid"
@@ -188,10 +189,15 @@ func persistentClusterID() string {
 	// If there is not a database file in use, we want a cluster ID to be the same
 	// for every re-run, so we set it as an environment config in a special env
 	// file. We do not error if we can neither read nor write the file.
+
+	if id := os.Getenv("TEMPORAL_DEV_SERVER_CLUSTER_ID"); id != "" {
+		return id
+	}
+
 	file := defaultDeprecatedEnvConfigFile("temporalio", "version-info")
 	if file == "" {
 		// No file, can do nothing here
-		return uuid.NewString()
+		return "dev-server-" + uuid.NewString()
 	}
 	// Try to get existing first
 	env, _ := readDeprecatedEnvConfigFile(file)
@@ -199,7 +205,7 @@ func persistentClusterID() string {
 		return id
 	}
 	// Create and try to write
-	id := uuid.NewString()
+	id := "dev-server-" + uuid.NewString()
 	_ = writeDeprecatedEnvConfigFile(file, map[string]map[string]string{"default": {"cluster-id": id}})
 	return id
 }
